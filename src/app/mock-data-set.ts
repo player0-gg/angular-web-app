e9bb53a11f1b21e9e86030c895529e268b90eddb
player0 <player0.gg@gmail.com>
2020-04-12 09:13:34 +0200

    set comment to not nullable

--- src/app/mock-data-set.ts
+++ src/app/mock-data-set.ts
@@ -1,7 +1,7 @@
 import { Data } from './models/data';
 
 export const DATASET: Data[] = [
-    { id: 1, title: 'Data 1', content: 'content 1', comment: 'comment 1'},
-    { id: 2, title: 'Data 2', content: 'content 2', comment: 'comment 2'},
-    { id: 3, title: 'Data 3', content: 'content 3', comment: 'comment 3'},
+    { id: 1, title: 'Data 1', comment: 'comment 1', content: 'content 1'},
+    { id: 2, title: 'Data 2', comment: 'comment 2', content: 'content 2'},
+    { id: 3, title: 'Data 3', comment: 'comment 3', content: 'content 3'}
 ];
--- src/app/models/data.ts
+++ src/app/models/data.ts
@@ -1,6 +1,6 @@
 export interface Data {
     id: number;
     title: string;
+    comment: string;
     content: string;
-    comment?: string;
 }
