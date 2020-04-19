6524fddd1f0442838ba55dfaf74fafbb211a5524
player0 <player0.gg@gmail.com>
2020-04-19 13:00:44 +0200

    add logout button
    TODO: create new workspace component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router, private authService: AuthService){}
   title = 'angular-web-app';
+
+  isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
